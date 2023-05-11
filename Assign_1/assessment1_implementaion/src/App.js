fd3824e2f24ddbfd97f4874d7a96a0e7777a3b64
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-05-11 23:42:11 +0530

    Update App.js

--- Assign_1/assessment1_implementaion/src/App.js
+++ Assign_1/assessment1_implementaion/src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="Ap">
       <LoadingSpinner/>
-      <DragAndDrop/>
+//       <DragAndDrop/>
 //       {<Rating rating={2.5} />}
 //       {<Carosuel/>}
 //       {<FileUpload/>}
