cf97d9c600a16d0caae299e33b0d75481d69bfba
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-05-14 23:41:43 +0530

    Update DragAndDrop.jsx

--- Assign_1/assessment1_implementaion/src/Components/DragAndDrop.jsx
+++ Assign_1/assessment1_implementaion/src/Components/DragAndDrop.jsx
@@ -5,11 +5,7 @@ export const DragAndDrop = () => {
   return (
     <div>
         <dropable>
-            <ul>
-               
-               
-               
-            </ul>
+            
         </dropable>
         <dragable></dragable>
 
